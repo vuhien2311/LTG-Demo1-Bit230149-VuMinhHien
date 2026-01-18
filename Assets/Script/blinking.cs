4224db243f3cac4667acd12f5c3cdf4eaf30e06f
vuhien2311 <vuhien7789@gmail.com>
2026-01-18 21:53:04 +0700

    add

---
+++ Assets/Script/blinking.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+public class Blinking : MonoBehaviour
+
+{
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    spriteRenderer.enabled = !spriteRenderer.enabled;
+    }
+}
---
+++ Assets/Script/showlog.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class ShowLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Debug.Log("Update called! " + Time.frameCount);
+    }
+}
