d3ad35d574227e1a5ebf79edf4c660220eccb6df
vuhien2311 <vuhien7789@gmail.com>
2026-02-01 14:47:39 +0700

    hihu

--- Assets/Script/Shooting.cs
+++ Assets/Script/Shooting.cs
@@ -1,16 +1,31 @@
-
 using UnityEngine;
 
-public class Shooting : MonoBehaviour
+public class PlayerShooting : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    public GameObject bulletPrefabs;
+    public float shootingInterval;
+    public Vector3 bulletOffset;
+    private float lastBulletTime;
 
     void Update()
     {
-    if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-        Instantiate(bulletPrefab, transform.position,
-        transform.rotation);
+            UpdateFiring();
         }
+    }
+
+    private void UpdateFiring()
+    {
+        if (Time.time - lastBulletTime > shootingInterval)
+        {
+            ShootBullet();
+            lastBulletTime = Time.time;
+        }
+    }
+
+    private void ShootBullet()
+    {
+        var bullet = Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
     }
 }
